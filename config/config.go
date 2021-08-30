d2e9b2ec739d8bd97782ca72d9f6b09d323db0ac
bingyanglin <bingyang.lin@iota.org>
2021-08-30 20:41:24 +0800

    Add simulation stop threshold parameter

--- config/config.go
+++ config/config.go
@@ -1,13 +1,14 @@
 package config
 
 const (
-	NodesCount             = 10_000
-	NodesTotalWeight       = 100_000_000
-	ZipfParameter          = 0.9
-	MessageWeightThreshold = 0.5
-	TSA                    = "URTS"
-	TPS                    = 1000
-	DecelerationFactor     = 5.0 // The factor to control the speed in the simulation.
+	NodesCount              = 10_000
+	NodesTotalWeight        = 100_000_000
+	ZipfParameter           = 0.9
+	MessageWeightThreshold  = 0.5
+	TSA                     = "URTS"
+	TPS                     = 1000
+	DecelerationFactor      = 5.0 // The factor to control the speed in the simulation.
+	SimulationStopThreshold = 0.5 // Stop the simulation when > SimulationStopThreshold * NodesCount have reached the same opinion.
 )
 
 var (

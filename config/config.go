8b34c6842d5d4c977446e9f2c29b2f2be0779331
bingyanglin <bingyang.lin@iota.org>
2021-09-01 18:45:27 +0800

    Change the config for CT sim

--- config/config.go
+++ config/config.go
@@ -1,16 +1,16 @@
 package config
 
 var (
-	NodesCount              = 1_000
+	NodesCount              = 100
 	NodesTotalWeight        = 100_000_000
-	ZipfParameter           = 0.9
+	ZipfParameter           = 2.0
 	MessageWeightThreshold  = 0.5
-	TipsCount               = 4    // The TipsCount for a message
-	WeakTipsRatio           = 0.25 // The ratio of weak tips
+	TipsCount               = 4   // The TipsCount for a message
+	WeakTipsRatio           = 0.0 // The ratio of weak tips
 	TSA                     = "URTS"
-	TPS                     = 1000
-	DecelerationFactor      = 2.0  // The factor to control the speed in the simulation.
-	ConsensusMonitorTick    = 100  // Tick to monitor the consensus, in milliseconds.
+	TPS                     = 100
+	DecelerationFactor      = 1.0  // The factor to control the speed in the simulation.
+	ConsensusMonitorTick    = 1000 // Tick to monitor the consensus, in milliseconds.
 	ReleventValidatorWeight = 1000 // The node whose weight * ReleventValidatorWeight <= largestWeight will not issue messages
 )
 

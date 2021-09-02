21ff0d9cae71e100aa541f0464c3221da50336ec
bingyanglin <bingyang.lin@iota.org>
2021-09-02 14:43:27 +0800

    Save the config for ct simulation

--- config/config.go
+++ config/config.go
@@ -1,17 +1,20 @@
 package config
 
 var (
-	NodesCount              = 100
+	NodesCount              = 200
 	NodesTotalWeight        = 100_000_000
-	ZipfParameter           = 2.0
+	ZipfParameter           = 0.9
 	MessageWeightThreshold  = 0.5
 	TipsCount               = 4   // The TipsCount for a message
 	WeakTipsRatio           = 0.0 // The ratio of weak tips
 	TSA                     = "URTS"
-	TPS                     = 100
+	TPS                     = 1000
 	DecelerationFactor      = 1.0  // The factor to control the speed in the simulation.
 	ConsensusMonitorTick    = 1000 // Tick to monitor the consensus, in milliseconds.
 	ReleventValidatorWeight = 1000 // The node whose weight * ReleventValidatorWeight <= largestWeight will not issue messages
+	PayloadLoss             = 0.0  // The payload loss in the network
+	MinDelay                = 0    // The minimum network delay in ms
+	MaxDelay                = 0    // The maximum network delay in ms
 )
 
 var (

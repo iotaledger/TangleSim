2f8418faa0c997deed5615103cf3739df1459495
Bing-Yang <51323441+bingyanglin@users.noreply.github.com>
2021-09-23 15:50:00 +0800

    Update the getter naming.
    Co-authored-by: Daria Dziubałtowska <44535712+daria305@users.noreply.github.com>

--- main.go
+++ main.go
@@ -207,7 +207,7 @@ func dumpNetwork(net *network.Network, fileName string) {
 			record := []string{
 				strconv.FormatInt(int64(peer.ID), 10),
 				strconv.FormatInt(int64(neighbor), 10),
-				strconv.FormatInt(connection.GetNetworkDelay().Nanoseconds(), 10),
+				strconv.FormatInt(connection.NetworkDelay().Nanoseconds(), 10),
 				strconv.FormatInt(int64(connection.GetPacketLoss()*100), 10),
 				strconv.FormatInt(int64(net.WeightDistribution.Weight(peer.ID)), 10),
 			}

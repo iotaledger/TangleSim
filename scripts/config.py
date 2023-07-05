0d8d693862e40217d9b6fc7bc66810217b43bc12
jkrvivian <jkrvivian@gmail.com>
2023-07-05 14:33:52 +0800

    Comment hack in config.py

--- scripts/config.py
+++ scripts/config.py
@@ -23,7 +23,7 @@ class Configuration:
         self.cd['MULTIVERSE_PATH'] + "/results"
         self.cd['SCRIPT_START_TIME'] = datetime.strftime(
             datetime.now(), "%Y%m%d_%H%M")
-        self.cd['SCRIPT_START_TIME'] = '20230705_0905'
+        # self.cd['SCRIPT_START_TIME'] = '20230705_0905'
         self.cd['CONFIGURATION_PATH'] = f"{self.cd['MULTIVERSE_PATH']}/results/{self.cd['SCRIPT_START_TIME']}/mb.config"
         self.cd['GENERAL_OUTPUT_PATH'] = f"{self.cd['MULTIVERSE_PATH']}/results/{self.cd['SCRIPT_START_TIME']}/general"
         self.cd['SCHEDULER_OUTPUT_PATH'] = f"{self.cd['MULTIVERSE_PATH']}/results/{self.cd['SCRIPT_START_TIME']}/scheduler"

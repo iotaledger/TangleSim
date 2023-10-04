417fdaf715bbe40aa8b68ccdf14c1e11ae2980a6
jkrvivian <jkrvivian@gmail.com>
2023-10-04 14:03:44 +0800

    Add more events for acceptance and confirmation

--- multiverse/approvalweight_manager.go
+++ multiverse/approvalweight_manager.go
@@ -19,18 +19,15 @@ func NewApprovalManager(tangle *Tangle) *ApprovalManager {
 	return &ApprovalManager{
 		tangle: tangle,
 		Events: &ApprovalWeightEvents{
-			// TODO (confirmation): rename MessageConfirmed to be 1) MessageAccepted
-			//                     add 2) MessagePreAccepted
-			//                         3) MessagePreConfirmed
-			//                         4) MessageConfirmed
-			//                     rename MessageWeightUpdated to be 1) MessagePreAcceptanceWeightUpdated
-			//                     add 2) MessageAcceptanceWeightUpdated
-			//                         3) MessagePreConfirmationWeightUpdated
-			//                         4) MessageConfirmationWeightUpdated
-			//                     note that MessageWitnessWeightUpdated is only for metrics
-			MessageConfirmed:            events.NewEvent(approvalEventCaller),
-			MessageWeightUpdated:        events.NewEvent(approvalEventCaller),
-			MessageWitnessWeightUpdated: events.NewEvent(witnessWeightEventCaller),
+			MessagePreAccepted:                  events.NewEvent(approvalEventCaller),
+			MessageAccepted:                     events.NewEvent(approvalEventCaller),
+			MessagePreConfirmed:                 events.NewEvent(approvalEventCaller),
+			MessageConfirmed:                    events.NewEvent(approvalEventCaller),
+			MessagePreAcceptanceWeightUpdated:   events.NewEvent(approvalEventCaller),
+			MessageAcceptanceWeightUpdated:      events.NewEvent(approvalEventCaller),
+			MessagePreConfirmationWeightUpdated: events.NewEvent(approvalEventCaller),
+			MessageConfirmationWeightUpdated:    events.NewEvent(approvalEventCaller),
+			MessageWitnessWeightUpdated:         events.NewEvent(witnessWeightEventCaller),
 		},
 	}
 }
@@ -44,19 +41,16 @@ func witnessWeightEventCaller(handler interface{}, params ...interface{}) {
 }
 
 func (a *ApprovalManager) Setup() {
-	a.tangle.Solidifier.Events.MessageSolid.Attach(events.NewClosure(a.ApproveMessages))
+	a.tangle.Solidifier.Events.MessageSolid.Attach(events.NewClosure(a.TrackAcceptanceWeight))
 }
 
-// TODO (confirmation): Rename ApproveMessages to be 1) TrackAcceptanceWeight
 // TODO (confirmation): Add a similar function: 2) TrackPreAcceptanceWeight
 // TODO (confirmation): Add a similar function: 3) TrackPreConfirmationWeight
 // TODO (confirmation): Add a similar function: 4) TrackConfirmationWeight
-func (a *ApprovalManager) ApproveMessages(messageID MessageID) {
-
+func (a *ApprovalManager) TrackAcceptanceWeight(messageID MessageID) {
 	issuingMessage := a.tangle.Storage.Message(messageID)
 	byteIndex := issuingMessage.Issuer / 8
-	// TODO (confirmation): rename mode to be nodeBitmapBitLocation
-	mod := issuingMessage.Issuer % 8
+	nodeBitmapBitLocation := issuingMessage.Issuer % 8
 
 	if !issuingMessage.Validation {
 		return
@@ -68,22 +62,13 @@ func (a *ApprovalManager) ApproveMessages(messageID MessageID) {
 			// log.Infof("Peer %d Message %d Witness Weight %d", a.tangle.Peer.ID, messageMetadata.id, messageMetadata.weight)
 			a.Events.MessageWitnessWeightUpdated.Trigger(message, messageMetadata.Weight())
 		}
-		// TODO (confirmation): weightByte to be nodeBitmapByte
-		//                     The byte is to record which node has already accounted for the weight of this message
-		//                     The byte only record the 8 corresponding nodes
-		// Ex: 16 nodes, Byte 0, Byte 1, Byte 2 Byte 3
-		//               For each byte, we have 8 bits
-		//               the 1st bit in Byte 0 is for node 0
-		//               the 2nd bit in Byte 0 is for node 1
-		//               the 3rd bit in Byte 0 is for node 2
-		//               the 4th bit in Byte 0 is for node 3
-		//                ...
-		weightByte := messageMetadata.PreAcceptedBitmapByte(int(byteIndex))
-		if weightByte&(1<<mod) == 0 {
-			weightByte |= 1 << mod
-			messageMetadata.SetPreAcceptedBitmapByte(int(byteIndex), weightByte)
+
+		nodeBitmapByte := messageMetadata.PreAcceptedBitmapByte(int(byteIndex))
+		if nodeBitmapByte&(1<<nodeBitmapBitLocation) == 0 {
+			nodeBitmapByte |= 1 << nodeBitmapBitLocation
+			messageMetadata.SetPreAcceptedBitmapByte(int(byteIndex), nodeBitmapByte)
 			messageMetadata.AddWeight(weight)
-			a.Events.MessageWeightUpdated.Trigger(message, messageMetadata, messageMetadata.Weight())
+			a.Events.MessagePreAcceptanceWeightUpdated.Trigger(message, messageMetadata, messageMetadata.Weight())
 			if float64(messageMetadata.Weight()) >= config.Params.ConfirmationThreshold*float64(a.tangle.WeightDistribution.TotalWeight()) &&
 				!messageMetadata.Confirmed() && !messageMetadata.Orphaned() {
 				// check if this should be orphaned
@@ -111,9 +96,15 @@ func (a *ApprovalManager) ApproveMessages(messageID MessageID) {
 // region ApprovalWeightEvents /////////////////////////////////////////////////////////////////////////////////////////////
 
 type ApprovalWeightEvents struct {
-	MessageConfirmed            *events.Event
-	MessageWeightUpdated        *events.Event
-	MessageWitnessWeightUpdated *events.Event
+	MessagePreAccepted                  *events.Event
+	MessageAccepted                     *events.Event
+	MessagePreConfirmed                 *events.Event
+	MessageConfirmed                    *events.Event
+	MessagePreAcceptanceWeightUpdated   *events.Event
+	MessageAcceptanceWeightUpdated      *events.Event
+	MessagePreConfirmationWeightUpdated *events.Event
+	MessageConfirmationWeightUpdated    *events.Event
+	MessageWitnessWeightUpdated         *events.Event
 }
 
 // endregion ///////////////////////////////////////////////////////////////////////////////////////////////////////////
